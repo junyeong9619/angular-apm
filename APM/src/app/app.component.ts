6e79a8d1683020b0a531e2bd9d193fd0c545a13c
Lee Jun Yeong <junyeong9619@gmail.com>
2018-01-28 22:24:08 +0800

    Initial Commit

---
+++ APM/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ProductService } from './product/product.service';
+
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
+})
+export class AppComponent {
+  title = 'Angular: Getting Started';
+}
---
+++ APM/src/app/product/product-list.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './products';
+import { IPromise } from 'q';
+import { ProductService } from './product.service';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+    constructor(private _productService: ProductService) {
+
+    }
+
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    errorMessage: string;
+
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    ngOnInit(): void {
+        this._productService.getProducts().subscribe(products => {
+            this.products = products;
+            this.filteredProducts = this.products;
+        }, error => this.errorMessage = <any>error);
+    }
+
+    onRatingClicked(msg: string): void {
+        this.pageTitle = msg;
+    }
+    filteredProducts: IProduct[];
+    products: IProduct[] = [];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+}
